4fe52889b1024746f663202f2c60766d5f7c025b
Bryson-V <97585647+Bryson-V@users.noreply.github.com>
2023-01-17 09:44:31 -0800

    Delete server directory

--- server/Express.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const nodemailer = require("nodemailer");
-const app = express();
-require("dotenv").config();
-
-const port = 3001;
-app.listen(port, () => {
- console.log(`Server is running on port: ${port}`);
-});
